b3441d90144f3b38e2afa5c4ca1d9ccb054c5bde
Jens Van Durme <jens.vandurme@student.hogent.be>
2026-03-13 10:01:25 +0100

    Update create table sql file

---
+++ SQL_Create-table.sql
@@ -0,0 +1,14 @@
+drop table if exists Klant;
+drop table if exists Adres;
+
+create table Klant(
+id int identity(1,1) not null,
+voornaam nvarchar(50) not null,
+familienaam nvarchar(50) not null,
+email nvarchar(100) not null,
+geboortedatum Date not null,
+postcode nvarchar(10) not null,
+gemeente nvarchar(50) not null,
+straat nvarchar(100) not null,
+huisnr nvarchar(5) not null
+);
